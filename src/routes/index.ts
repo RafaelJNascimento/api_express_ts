e7f5a01761379cc231b2e2c4c884dd11388ed91f
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-12-17 11:24:14 -0300

    3 - Criação de Registros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,36 +1,10 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { parseISO } from 'date-fns';
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-import CreateAppointmentService from '../services/CreateAppointmentService';
+const routes = Router();
 
-const appointmentsRouter = Router();
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
-
-  return response.json(appointments);
-});
-
-appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = new CreateAppointmentService();
-
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider,
-    });
-
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
-});
-
-export default appointmentsRouter;
+export default routes;
